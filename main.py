4b1ad0023dd43a8c5013d732d9c24a93a357e8f0
newsha varnaseri <nioshavarnaseri@gmail.com>
2026-07-12 23:53:03 +0330

    refactor showing results

--- main.py
+++ main.py
@@ -7,46 +7,48 @@ words = ['hello', 'world', 'piano', 'train', 'brave', 'roast',
 rounds = 5
 won = False
 answer = random.choice(words)
-WORD_LENGTH = len(answer)
 
 def get_input():
     guess = input("enter your guess: ").strip().lower()
-    while len(guess) != WORD_LENGTH:
-        print(f"the word must have {WORD_LENGTH} letters!")
+    while len(guess) != len(answer):
+        print(f"the word must have {len(answer)} letters!")
         guess = input("try again: ").strip().lower()
     return guess
 
 def check_guess(guess: str, answer: str):
-    result = [0] * WORD_LENGTH
+    result = [0] * len(answer)
 
-    for i in range(WORD_LENGTH):
+    for i in range(len(answer)):
         if guess[i] == answer[i]:
             result[i] = 1
         elif guess[i] in answer:
             result[i] = 2
 
     return result
-        
-while(rounds != 0):
-    
-    guess = get_input()
-    result = check_guess(guess, answer)   
-    
 
-    for i in range(WORD_LENGTH):
+def show_result(guess: str, result):
+    for i in range(len(guess)):
         if result[i] == 1:
             print(colored(guess[i], "green"), end="")
         elif result[i] == 2:
             print(colored(guess[i], "yellow"), end="")
         else:
-            print(colored(guess[i], "red"), end="")
-    
+            print(colored(guess[i], "red"), end="") 
+             
+def show_end(won: bool, answer: str):
+    if won:
+        print(colored("YOU WON!!!", "green"))
+    else:
+        print(colored(f"YOU LOSE!!! Answer was '{answer}'", "red"))
+        
+while(rounds != 0): 
+    guess = get_input()
+    result = check_guess(guess, answer)   
+    show_result(guess, result)
     rounds-=1
     print("\n")
     if guess == answer:
-        print(colored('YOU won!!!', "green"))
         won = True
         break
     
-if not won:
-    print(colored(f"YOU LOSE!!! Answer was '{answer}'", "red"))        
+show_end(won, answer)
